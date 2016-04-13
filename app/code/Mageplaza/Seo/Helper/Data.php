10a3ad8e71079ae7d217dc159228e92b4b3fe559
KiuNguyen <vn.ngocdung@gmail.com>
2016-04-13 11:38:16 +0700

    add helper config

--- app/code/Mageplaza/Seo/Helper/Data.php
+++ app/code/Mageplaza/Seo/Helper/Data.php
@@ -9,4 +9,88 @@ use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
+    const XML_PATH_GENERAL = 'seo/general/';
+    const XML_PATH_META = 'seo/meta/';
+    const XML_PATH_RICHSNIPPETS = 'seo/richsnippets/';
+    const XML_PATH_HTACCESS = 'seo/htaccess/';
+    const XML_PATH_ROBOTS = 'seo/robots/';
+    const XML_PATH_HTML_SITEMAP = 'seo/htmlsitemap/';
+    protected $storeManager;
+    protected $objectManager;
+
+    public function __construct(
+        Context $context,
+        ObjectManagerInterface $objectManager,
+        StoreManagerInterface $storeManager
+    )
+    {
+        $this->objectManager = $objectManager;
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
+    }
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    public function getGeneralConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_GENERAL . $code, $storeId);
+    }
+
+    public function getMetaConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_META . $code, $storeId);
+    }
+
+    public function getRichsnippetsConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_RICHSNIPPETS . $code, $storeId);
+    }
+
+    public function getHtaccessConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_HTACCESS . $code, $storeId);
+    }
+
+    public function getRobotsConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_ROBOTS . $code, $storeId);
+    }
+
+    public function getHtmlsitemapConfig($code, $storeId = null)
+    {
+        return $this->getConfigValue(self::XML_PATH_HTML_SITEMAP . $code, $storeId);
+    }
+
+    /**
+     * convert array to options
+     *
+     * @access public
+     * @param $options
+     * @return array
+     * @author Sam
+     */
+    public function convertOptions($options)
+    {
+        $converted = array();
+        foreach ($options as $option) {
+            if (isset($option['value']) && !is_array($option['value']) &&
+                isset($option['label']) && !is_array($option['label'])
+            ) {
+                $converted[$option['value']] = $option['label'];
+            }
+        }
+        return $converted;
+    }
+
+    public function getHtmlSitemapUrl()
+    {
+        $this->_getUrl('mageplaza_seo/sitemap');
+    }
 }
