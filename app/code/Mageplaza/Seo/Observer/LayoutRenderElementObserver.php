b276585b4552dff03aa1a0e8f3294a87ff720405
KiuNguyen <vn.ngocdung@gmail.com>
2016-04-23 10:22:34 +0700

    fix bug

---
+++ app/code/Mageplaza/Seo/Observer/LayoutRenderElementObserver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Mageplaza\Seo\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Mageplaza\Seo\Helper\Data as SeoHelper;
+use Magento\Framework\Registry;
+use Magecheckout\Seo\Model\Source\Robots;
+class LayoutRenderElementObserver implements ObserverInterface
+{
+    protected $helper;
+    protected $registry;
+    public function __construct(
+        SeoHelper $helper,
+        Registry $registry
+    )
+    {
+        $this->helper = $helper;
+        $this->registry = $registry;
+    }
+
+    public function execute(Observer $observer)
+    {
+//        $elementName=$observer->getEvent()->getElementName();
+//        $layout=$observer->getEvent()->getLayout();
+//        $transport=$observer->getEvent()->getTransport();
+//        \Zend_Debug::dump($elementName);
+//        \Zend_Debug::dump($transport->getHtml());die;
+    }
+
+
+}
