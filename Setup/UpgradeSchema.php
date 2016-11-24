5c53d74bf5a6330c3d2c0c9bf34c179a44bad723
Sam <sam@mageplaza.com>
2016-11-24 18:52:31 +0700

    fix Upgrade issue

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -2,25 +2,14 @@
 
 namespace Mageplaza\Seo\Setup;
 
-use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
-use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
 
 
 class UpgradeSchema implements UpgradeSchemaInterface
 {
-    private $eavSetupFactory;
 
-    /**
-     * UpgradeData constructor.
-     *
-     * @param ModuleDataSetupInterface $eavSetupFactory
-     */
-    public function __construct(ModuleDataSetupInterface $eavSetupFactory)
-    {
-        $this->eavSetupFactory = $eavSetupFactory;
-    }
 
     /**
      * upgrade
@@ -30,68 +19,8 @@ class UpgradeSchema implements UpgradeSchemaInterface
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $setup->startSetup();
-
-        if (version_compare($context->getVersion(), '1.1.0', '<')) {
-            $this->_addRobotsAttribute($setup);
-        }
-
         $setup->endSetup();
     }
 
-    /**
-     * add robots attribute to Product, Category
-     * @param $setup
-     */
-    protected function _addRobotsAttribute($setup){
-        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
 
-        $eavSetup->addAttribute(
-            \Magento\Catalog\Model\Product::ENTITY,
-            'mp_meta_robots',
-            [
-                'type' => 'varchar',
-                'backend' => '',
-                'frontend' => '',
-                'label' => 'Meta Robots',
-                'note'=> 'Added by Mageplaza.com',
-                'input' => 'select',
-                'class' => '',
-                'source' => '\Mageplaza\Seo\Model\Source\Robots',
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                'visible' => true,
-                'required' => false,
-                'user_defined' => false,
-                'default' => '',
-                'searchable' => false,
-                'filterable' => false,
-                'comparable' => false,
-                'visible_on_front' => false,
-                'used_in_product_listing' => true,
-                'unique' => false,
-                'group' => 'Search Engine Optimization',
-                'sort_order' => 100,
-                'apply_to' => '',
-            ]
-        );
-
-        $eavSetup->addAttribute(
-            \Magento\Catalog\Model\Category::ENTITY,
-            'mp_meta_robots',
-            [
-                'type' => 'varchar',
-                'label' => 'Meta Robots',
-                'note'=> 'Added by Mageplaza.com',
-                'input' => 'select',
-                'sort_order' => 41,
-                'source' => '\Mageplaza\Seo\Model\Source\Robots',
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
-                'visible' => true,
-                'required' => false,
-                'user_defined' => false,
-                'default' => null,
-                'group' => 'General Information',
-                'backend' => ''
-            ]
-        );
-    }
 }
