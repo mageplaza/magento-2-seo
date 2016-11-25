a9b9933cfb130f4ea472d9c399799e2f07749763
Sona <lyhuonghanu@gmail.com>
2016-11-25 07:12:06 +0700

    Fix upgrade issue

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -1,25 +1,19 @@
 <?php
-
 namespace Mageplaza\Seo\Setup;
 
 use Magento\Framework\Setup\UpgradeSchemaInterface;
-use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-
-
+use Magento\Framework\Setup\ModuleContextInterface;
 class UpgradeSchema implements UpgradeSchemaInterface
 {
-    private $eavSetupFactory;
-
-
-    /**
-     * upgrade
-     * @param SchemaSetupInterface   $setup
-     * @param ModuleContextInterface $context
-     */
-    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
-    {
-        $setup->startSetup();
-        $setup->endSetup();
-    }
+	/**
+	 * upgrade
+	 * @param SchemaSetupInterface   $setup
+	 * @param ModuleContextInterface $context
+	 */
+	public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+	{
+		$setup->startSetup();
+		$setup->endSetup();
+	}
 }
