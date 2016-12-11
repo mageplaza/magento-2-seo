f493b5f4bc3ea7e47dccc665dda44460ef414e59
Sona <lyhuonghanu@gmail.com>
2016-12-11 15:26:05 +0700

    add stopword list

---
+++ Helper/StopWords.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Mageplaza\Seo\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class StopWords extends AbstractHelper
+{
+
+	/**
+	 * get stop words
+	 * @return array
+	 */
+	public function getStopWords()
+	{
+		return [];
+	}
+
+}
