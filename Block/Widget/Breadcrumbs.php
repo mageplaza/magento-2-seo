41726443b2c11224bc4168ebd45ebaf970444675
trungtong <trungtt@mageplaza.com>
2017-03-11 11:41:44 +0700

    compile

--- Block/Widget/Breadcrumbs.php
+++ Block/Widget/Breadcrumbs.php
@@ -1,7 +1,6 @@
 <?php
 namespace Mageplaza\Seo\Block\Widget;
 
-use Mageplaza\Seo\Helper\Data as HelperData;
 use Magento\Backend\Block\Widget\Breadcrumbs as WidgetBreadcrumbs;
 
 class Breadcrumbs extends WidgetBreadcrumbs
@@ -11,29 +10,5 @@ class Breadcrumbs extends WidgetBreadcrumbs
 	 */
 	protected $_template = 'Mageplaza_Seo::richsnippets/jsonld/breadcrumb.phtml';
 
-	private $_helperData;
-
-	/**
-	 * @return void
-	 */
-	protected function _construct(
-		HelperData $helperData
-	)
-	{
-		$this->_helperData = $helperData;
-
-//		return parent::_construct();
-	}
-
-
-	/**
-	 * get seo helper
-	 *
-	 * @return \Mageplaza\Seo\Helper\Data
-	 */
-	public function getHelper()
-	{
-		return $this->_helperData;
-	}
 
 }
