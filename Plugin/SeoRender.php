992d2293bb7c35fa02dcd9c1d98158c7531db8a5
Bùi Đức Trung <trungbd@mageplaza.com>
2019-08-01 09:10:29 +0700

    update schema data

--- Plugin/SeoRender.php
+++ Plugin/SeoRender.php
@@ -445,7 +445,7 @@ class SeoRender
                             'author' => $review['nickname']
                         ];
                     }
-                } elseif (!$this->helperData->getRichsnippetsConfig('aggregate_rating')) {
+                } elseif ($this->helperData->getRichsnippetsConfig('aggregate_rating') === '1') {
                     $productStructuredData['review'][] = [
                         '@type'  => 'Review',
                         'author' => $this->helperData->getRichsnippetsConfig('review_author')
